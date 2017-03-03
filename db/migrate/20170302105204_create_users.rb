e8209f7354bbc1c202ee65f5de6dbf33dac64b3f
Sarah Dudek <email": null>
2017-03-02 18:29:45 -0600

    Add google api

--- db/migrate/20170302105204_create_users.rb
+++ db/migrate/20170302105204_create_users.rb
@@ -1,16 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name, :null => false
-      t.string :last_name, :null => false
-      t.string :username, :null => false
+      t.string :name, :null => false
+      t.string :username
       t.string :email, :null => false
-      t.datetime :birthdate, :null => false
-      t.string :gender, :null => false
+      t.datetime :birthdate
+      t.string :gender
       t.string :hashed_password, :null => false
 
       t.timestamps null:false
     end
-    add_index :users, :username, :unique => true
+    add_index :users, :email, :unique => true
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,33 +1,3 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-
-});
-
-function connectToFacebook(){
-  FB.Event.subscribe('auth.authResponseChange', auth_response_change_callback);
-  FB.Event.subscribe('auth.statusChange', auth_status_change_callback);
-}
-
-
-var auth_response_change_callback = function(response) {
-  console.log("auth_response_change_callback");
-  console.log(response);
-}
-
-var auth_status_change_callback = function(response) {
-  FB.getLoginStatus(function(response) {
-      if (response.status === 'connected'){
-        // send them to their user profile
-        console.log("you're connected, bitch")
-        var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
-        console.log(accessToken)
-      } else if (response.status === 'not_authorized') {
-        // send the thing to get permission to use their fb
-        console.log("here")
-      } else {
-        console.log("there")
-      }
-  });
-  console.log("auth_status_change_callback: " + response.status);
-}
+})
